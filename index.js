e6d3395faa998f936ee77f128a1ee949df0d3bd4
Nisarg Kapkar <53402062+Nkap23@users.noreply.github.com>
2020-10-26 11:46:18 +0530

    updated about page with hide/unhide

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.getElementById("downres").onclick
+function hidee(obj)
+{
+    var el1=document.getElementById('resumeb');
+    el1.style.display='none';
+    var el2=document.getElementById('bottomm');
+    el2.style.display='block';
+}
