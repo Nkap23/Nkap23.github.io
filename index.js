ce5704ab3156df17813bf8d2f5a783aa5fde5e4f
Nisarg Kapkar <53402062+Nkap23@users.noreply.github.com>
2020-11-01 19:17:50 +0530

    added background and filled some resume

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function hidee(obj)
     var el1=document.getElementById('resumeb');
     el1.style.display='none';
     var el2=document.getElementById('bottomm');
-    el2.style.display='flex';
+    el2.style.display='block';
 }
