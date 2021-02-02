df16b4c988e56e362cfd07d9a1f3b1382ce89cc7
Nisarg Kapkar <53402062+Nkap23@users.noreply.github.com>
2021-02-02 10:10:09 +0530

    added subheadings

---
+++ portfolio/portfolio.js
@@ -0,0 +1,45 @@
+$("#scroll1").click(function(){
+    $('html, body').animate({
+        scrollTop:($("#about").offset().top-$("#intro").offset().top)
+    },1500);
+});
+$("#scroll2").click(function(){
+    $('html, body').animate({
+        scrollTop:($("#education").offset().top-$("#intro").offset().top)
+    },1500);
+});
+$("#scroll3").click(function(){
+    $('html, body').animate({
+        scrollTop:($("#skills").offset().top-$("#intro").offset().top)
+    },1500);
+});
+$("#scroll4").click(function(){
+    $('html, body').animate({
+        scrollTop:($("#experience").offset().top-$("#intro").offset().top)
+    },1500);
+});
+$("#scroll5").click(function(){
+    $('html, body').animate({
+        scrollTop:($("#projects").offset().top-$("#intro").offset().top)
+    },1500);
+});
+$("#scroll6").click(function(){
+    $('html, body').animate({
+        scrollTop:($("#blogs").offset().top-$("#intro").offset().top)
+    },1500);
+});
+$("#scroll7").click(function(){
+    $('html, body').animate({
+        scrollTop:($("#publications").offset().top-$("#intro").offset().top)
+    },1500);
+});
+$("#scroll8").click(function(){
+    $('html, body').animate({
+        scrollTop:($("#courses").offset().top-$("#intro").offset().top)
+    },1500);
+});
+$("#scroll9").click(function(){
+    $('html, body').animate({
+        scrollTop:($("#achievments").offset().top-$("#intro").offset().top)
+    },1500);
+});
